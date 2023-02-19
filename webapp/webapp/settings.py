22b7fe51551cfe9530ffeea3ef178b358eb0c35e
AdityaAshokan <74782791+AdityaAshokan@users.noreply.github.com>
2023-02-20 00:37:51 +0530

    Update settings.py

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+50p0#opaj&bf-x=_k!)l!q!^(-9yvf%ew4qv!w5cx%*75mnbp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [54.199.50.151]
+ALLOWED_HOSTS = []
 
 
 # Application definition
